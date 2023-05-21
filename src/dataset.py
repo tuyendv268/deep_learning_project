974ef97cf62ad368ee2f2380e4e5f17f4f60057c
tuyendv <tuyen.duongvan268@gmail.com>
2023-05-21 22:45:21 +0700

    update augment data

--- src/dataset.py
+++ src/dataset.py
@@ -1,6 +1,32 @@
 from torch.utils.data import Dataset
 import torch
 from torchvision.io import read_image
+from torchvision.utils import make_grid
+from torchvision import transforms
+import torchvision
+import PIL
+from imgaug import augmenters as iaa
+import imgaug as ia
+from torchvision.io import read_image
+import numpy as np
+import imageio
+
+class ImgAugTransform:
+    def __init__(self):
+        self.aug = iaa.Sequential([
+            # iaa.Scale((256, 256)),
+            iaa.Sometimes(0.25, iaa.GaussianBlur(sigma=(0, 3.0))),
+            iaa.Fliplr(0.5),
+            iaa.Affine(rotate=(-20, 20), mode='symmetric'),
+            iaa.Sometimes(0.25,
+                          iaa.OneOf([iaa.Dropout(p=(0, 0.1)),
+                                     iaa.CoarseDropout(0.1, size_percent=0.5)])),
+            iaa.AddToHueAndSaturation(value=(-10, 10), per_channel=True)
+        ], random_order=True)
+      
+    def __call__(self, img):
+        img = np.array(img)
+        return self.aug.augment_image(img)
 
 class ImageDataset(Dataset):
     def __init__(self, datas, config) -> None:
@@ -10,6 +36,7 @@ class ImageDataset(Dataset):
         self.label_names = config["labels"]
         self.max_width = config["max_width"]
         self.max_height = config["max_height"]
+        self.aug_img = ImgAugTransform()
         
     def __len__(self):
         return self.datas.shape[0]
@@ -19,10 +46,15 @@ class ImageDataset(Dataset):
         image = image[:, 0:self.max_height, 0:self.max_width]
         label = torch.tensor(label).unsqueeze(-1)
         
+        image = self.aug_img(image.permute(1, 2, 0))
+        image = torch.tensor(image)
+        image = image.permute(2, 0, 1)
+
         return image.float(), label
     
     def __getitem__(self, index) -> None:
         image_path = self.datas["id"][index]
+
         label = self.datas[self.label_names].iloc[index].tolist()
         
         return self._parse_sample(image_path, label)
